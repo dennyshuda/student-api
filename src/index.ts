56a6d563709dd7df056767443851848d78c83803
Denny Setyawan Huda <dennshgit@gmail.com>
2023-04-19 21:49:14 +0700

    feat: import middleware and student routes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express, { Application, Request, Response } from "express";
-import { HealthRouter } from "./routes/health.route";
+import cors from "cors";
+import "./utils/connectDB";
+import StudentRouter from "../src/routes/student.route";
+import HealthRouter from "./routes/health.route";
 
 const PORT: Number = 8000;
 const app: Application = express();
 
+app.use(cors());
+app.use(express.json());
+
 app.use(HealthRouter);
+app.use(StudentRouter);
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
