9d8fa59e879fc208d53d3be7ade5a889e41f8f20
Denny Setyawan Huda <denycam9@gmail.com>
2023-05-04 10:21:51 +0700

    feat: add user route

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import "./utils/connectDB";
 import StudentRouter from "../src/routes/student.route";
 import HealthRouter from "./routes/health.route";
+import UserRouter from "./routes/user.route";
 
 const PORT: Number = 8000;
 const app: Application = express();
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.use(HealthRouter);
 app.use(StudentRouter);
+app.use(UserRouter);
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
