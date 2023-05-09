14e67dd616a70f2de60471b0cdc09d123bf2d248
Denny Setyawan Huda <denycam9@gmail.com>
2023-05-09 11:40:47 +0700

    feat: user route

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -4,5 +4,6 @@ import userController from "../controllers/user.controller";
 const UserRouter = Router();
 
 UserRouter.post("/register", userController.register);
+UserRouter.post("/login", userController.login);
 
 export default UserRouter;
