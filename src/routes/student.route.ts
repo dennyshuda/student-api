13eecbbd6b5b4ffc3dde4f21aa4cb4a5b689fe7c
Denny Setyawan Huda <denycam9@gmail.com>
2023-05-09 11:41:47 +0700

    feat: add verifyToken in student router

--- src/routes/student.route.ts
+++ src/routes/student.route.ts
@@ -1,11 +1,20 @@
 import { Router } from "express";
 import studentController from "../controllers/students.controller";
+import verifyToken from "../middlewares/verifyToken";
 const StudentRouter = Router();
 
 StudentRouter.get("/students", studentController.getStudents);
-StudentRouter.post("/students", studentController.addStudent);
+StudentRouter.post("/students", verifyToken, studentController.addStudent);
 StudentRouter.get("/student/:id", studentController.getStudentById);
-StudentRouter.put("/student/:id", studentController.updateStudentById);
-StudentRouter.delete("/student/:id", studentController.deleteStudentById);
+StudentRouter.put(
+  "/student/:id",
+  verifyToken,
+  studentController.updateStudentById
+);
+StudentRouter.delete(
+  "/student/:id",
+  verifyToken,
+  studentController.deleteStudentById
+);
 
 export default StudentRouter;
